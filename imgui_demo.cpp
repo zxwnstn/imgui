ecd1efe60a2b4745d34b78312b49643bc379b373
omar <omarcornut@gmail.com>
2020-01-06 11:53:04 +0100

    Tables: Support for multi-line columns name. Renaming of some fields from BackupXXX to HostXXX. Comments.

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -3388,6 +3388,7 @@ static void ShowDemoWindowTables()
                 {
                     MyItem::s_current_sort_specs = sorts_specs; // Store in variable accessible by the sort function.
                     qsort(&items[0], (size_t)items.Size, sizeof(items[0]), MyItem::CompareWithSortSpecs);
+                    MyItem::s_current_sort_specs = NULL;
                 }
 
             // Display data
@@ -3576,6 +3577,7 @@ static void ShowDemoWindowTables()
             {
                 MyItem::s_current_sort_specs = sorts_specs; // Store in variable accessible by the sort function.
                 qsort(&items[0], (size_t)items.Size, sizeof(items[0]), MyItem::CompareWithSortSpecs);
+                MyItem::s_current_sort_specs = NULL;
             }
             items_need_sort = false;
 
